5bafc73b90fa38f0f94de764c726a0404396a0e9
Steven Lundy <steven.lundy@gmail.com>
2015-11-23 17:25:21 -0800

    Add linkedIn auth

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 var apiKeys = require('./apiKeys');
-var Linkedin = require('node-linkedin')(apiKeys.CLIENT_ID, apiKeys.CLIENT_SECRET, '/auth/linkedin/callback/');
+var Linkedin = require('node-linkedin')(apiKeys.CLIENT_ID, apiKeys.CLIENT_SECRET, 'http://localhost:3000/auth/linkedin/callback');
 
 var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.json());
+
+app.get('/auth/linkedin', function(req, res) {
+  var scope = ['r_basicprofile'];
+  Linkedin.auth.authorize(res, scope);
+});
+
+app.get('/auth/linkedin/callback/', function(req, res) {
+  Linkedin.auth.getAccessToken(res, req.query.code, req.query.state, function(err, results) {
+    if ( err ) {
+      return console.error(err);
+    }
+
+    console.log(results);
+    return res.redirect('/');
+  });
+});
 
 app.listen('3000', function () {
   console.log('listening on 3000');
